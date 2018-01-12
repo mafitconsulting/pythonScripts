11b22dd32956fe74afa6f6195357a0a39631ca5c
root <root@devtest.localdomain>
2018-01-12 15:51:43 +0000

    updated codecommit script

--- codecommit.py
+++ codecommit.py
@@ -5,6 +5,7 @@ import boto3
 import botocore
 import json
 import argparse
+import subprocess
 from switch import switch
 
 def list_repositories():
@@ -27,18 +28,36 @@ def get_repository(repository_name):
         response = client.get_repository(
                    repositoryName=repository_name
         )
-        print(response)
+        print(json.dumps(response, indent = 4, default=str))
     except botocore.exceptions.ClientError as e:
         logging.error(e.response['Error']['Code'])
 
-def create_repository(repository_name,repository_description)
+    return response
+
+def delete_repository(repository_name):
+    print("Deleting repository %s, are you sure you wish to proceed (y/n)" % repository_name)
+    rc = raw_input().lower()
+    if rc == 'y':
+        try:
+            object = get_repository(repository_name)
+            repository_id = object['repositoryMetadata']['repositoryId']
+            response = client.delete_repository(
+                       repositoryName=repository_id
+            )
+            print object['repositoryMetadata']['repositoryId']
+        except botocore.exceptions.ClientError as e:
+            print(e.response['Error']['Code'])
+    else:
+        sys.exit(1)
+
+def create_repository(repository_name,repository_description):
+    logging.info("Creating repository %s" % repository_name)
     try:
-        logging.info("Creating repository %s" % repository_name)
         response = client.create_repository(
-        repositoryName=repository_name
+        repositoryName=repository_name,
         repositoryDescription=repository_description
         )
-        print(response)
+        print(json.dumps(response, indent = 4, default=str))
     except botocore.exceptions.ClientError as e:
         logging.error(e.response['Error']['Code'])
 
@@ -46,8 +65,11 @@ def create_repository(repository_name,repository_description)
 parser = argparse.ArgumentParser()
 parser.add_argument("--getrepository", help="Requires the repository name")
 parser.add_argument("--listrepositories", help="List all repositories in codecommit", action='store_true')
-parser.add_argument("--createrepository","--description", help="Creates a codecommit repository", nargs='2')
+parser.add_argument("--createrepository", help="Creates a codecommit repository, requires --decription field")
+parser.add_argument("--deleterepository", help="Creates a codecommit repository, requires repository name")
+parser.add_argument("--description", help="Decription for code repository")
 args = parser.parse_args()
+
 
 # Sets up logging
 logging.basicConfig(filename='aws_codecommit.log',
@@ -64,7 +86,9 @@ if args.getrepository:
 elif args.listrepositories:
     list_repositories()
 elif args.createrepository:
-    create_repository(args.createrepository)
+    create_repository(args.createrepository,args.description)
+elif args.deleterepository:
+    delete_repository(args.deleterepository)
 
 
 
