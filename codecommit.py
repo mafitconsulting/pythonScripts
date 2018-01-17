f8292f6ae44d72aba261a92fbd25f3c448c376ad
root <root@devtest.localdomain>
2018-01-17 07:55:00 +0000

    updated codecommit

--- codecommit.py
+++ codecommit.py
@@ -1,22 +1,42 @@
 #!/bin/python
 import sys
 import logging
-import boto3
-import botocore
 import json
 import argparse
-import subprocess
-from switch import switch
+import boto3
+import botocore
+import random
+
+
+# CLI options
+parser = argparse.ArgumentParser()
+parser.add_argument("--getrepository", help="Requires the repository name")
+parser.add_argument("--listrepositories", help="List all repositories in codecommit", action='store_true')
+parser.add_argument("--createrepository", help="Creates a codecommit repository, requires --decription field")
+parser.add_argument("--deleterepository", help="Creates a codecommit repository, requires repository name")
+parser.add_argument("--description", help="Decription for code repository")
+parser.add_argument("--createbranch", help="Create new branch, requires description and repos name")
+args = parser.parse_args()
+
+
+# Sets up logging
+logging.basicConfig(filename='aws_codecommit.log',
+                   format='%(asctime)s - %(levelname)s: %(message)s',
+                   level=logging.DEBUG)
+
+# Sets resource s3 for use
+client = boto3.client('codecommit')
+
 
 def list_repositories():
     try:
         logging.info("Listing code commit repositories")
         print("Listing code commit repositories")
         response = client.list_repositories(
-                   sortBy='repositoryName',
-                   order='ascending'
-                   )
-        print(json.dumps(response, indent = 4))
+            sortBy='repositoryName',
+            order='ascending'
+        )
+        print(json.dumps(response, indent=4, default=str))
 
     except botocore.exceptions.ClientError as e:
         logging.error(e.response['Error']['Code'])
@@ -28,23 +48,24 @@ def get_repository(repository_name):
         response = client.get_repository(
                    repositoryName=repository_name
         )
-        print(json.dumps(response, indent = 4, default=str))
+        print(json.dumps(response, indent=4, default=str))
     except botocore.exceptions.ClientError as e:
         logging.error(e.response['Error']['Code'])
 
-    return response
 
 def delete_repository(repository_name):
-    print("Deleting repository %s, are you sure you wish to proceed (y/n)" % repository_name)
-    rc = raw_input().lower()
+    rc = raw_input("Deleting repository %s, are you sure you wish to proceed (y/n)" % repository_name).lower()
     if rc == 'y':
         try:
-            object = get_repository(repository_name)
-            repository_id = object['repositoryMetadata']['repositoryId']
+            #object = client.get_repository(
+            #         repositoryName=repository_name
+            #)
+            #repository_id = object['repositoryMetadata']['repositoryId']
             response = client.delete_repository(
-                       repositoryName=repository_id
+                       repositoryName=repository_name
             )
-            print object['repositoryMetadata']['repositoryId']
+            print(json.dumps(response, indent=4, default=str))
+            print "Repository Deleted"
         except botocore.exceptions.ClientError as e:
             print(e.response['Error']['Code'])
     else:
@@ -54,30 +75,27 @@ def create_repository(repository_name,repository_description):
     logging.info("Creating repository %s" % repository_name)
     try:
         response = client.create_repository(
-        repositoryName=repository_name,
-        repositoryDescription=repository_description
+             repositoryName=repository_name,
+             repositoryDescription=repository_description
         )
-        print(json.dumps(response, indent = 4, default=str))
+        print(json.dumps(response, indent=4, default=str))
     except botocore.exceptions.ClientError as e:
         logging.error(e.response['Error']['Code'])
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--getrepository", help="Requires the repository name")
-parser.add_argument("--listrepositories", help="List all repositories in codecommit", action='store_true')
-parser.add_argument("--createrepository", help="Creates a codecommit repository, requires --decription field")
-parser.add_argument("--deleterepository", help="Creates a codecommit repository, requires repository name")
-parser.add_argument("--description", help="Decription for code repository")
-args = parser.parse_args()
+def create_branch(repository_name, branch_name):
+    logging.info("Creating repository branch " % branch_name)
+    try:
+        commit = ''.join(random.choice('0123456789ABCDEFabcdef') for i in range(10))
+        response = client.create_branch(
+                   repositoryName=repository_name
+                   branchName=branch_name
+                   commitId=commit
+        )
+        print(json.dumps(response, indent=4, default=str))
+    except botocore.exceptions.ClientError as e:
+        logging.error(e.response['Error']['Code'])
 
-
-# Sets up logging
-logging.basicConfig(filename='aws_codecommit.log',
-                    format = '%(asctime)s - %(levelname)s: %(message)s',
-                    level=logging.DEBUG)
-
-# Sets resource s3 for use
-client = boto3.client('codecommit')
 
 # Dispatch structure for action
 
@@ -89,7 +107,5 @@ elif args.createrepository:
     create_repository(args.createrepository,args.description)
 elif args.deleterepository:
     delete_repository(args.deleterepository)
-
-
-
+elif args.create_branch(
 
